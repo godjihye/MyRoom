d5c88b95a04c8419ca5cd6b9dfc2c0fa3d452008
godjihye <njih112@gmail.com>
2024-11-14 15:48:25 +0900

    proj init

---
+++ MyRoomFE/MyRoomFE/MyRoomFEApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRoomFEApp.swift
+//  MyRoomFE
+//
+//  Created by jhshin on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MyRoomFEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
