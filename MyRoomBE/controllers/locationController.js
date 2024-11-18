db369c527c6a35f3ea91e754a87f957516252541
godjihye <njih112@gmail.com>
2024-11-18 10:30:36 +0900

    room 업데이트

--- MyRoomBE/controllers/locationController.js
+++ MyRoomBE/controllers/locationController.js
@@ -1,49 +1,49 @@
-const locationService = require("../services/locationService")
-const createLocation = async(req, res)=> {
-    try {
-        const location = locationService.createLocation(req.body)
-        res.status(201).json({data: location})
-    } catch(e) {
-        res.status(500).json({error: e.error})
-    }
-}
-const findAllLocation = async(req, res) => {
-    try {
-        const locations = locationService.findAllLocation(req.params.id)
-        res.status(200).json({data: locations})
-    } catch(e) {
-        res.status(500).json({error: e.error})
-    }
-}
-const findLocation = async(req, res)=> {
-    try {
-        const location = locationService.findLocation(req.params.id)
-        res.status(200).json({data: location})
-    } catch(e) {
-        res.status(500).json({error: e.error})
-    }
-}
-const deleteLocation = async(req,res)=> {
-    try {
-        locationService.deleteLocation(req.params.id)
-        res.status(202).json({success: "true"})
-    }catch(e){
-        res.status(500).json({error: e.error})
-    }
-}
-const updateLocation = async(req,res)=> {
-    try {
-        const result = locationService.updateLocation(req.params.id, req.body)
-        res.status(200).json({data: result})
-    }catch(e){
-        res.status(500).json({error: e.error})
-    }
-}
+const locationService = require("../services/locationService");
+const createLocation = async (req, res) => {
+  try {
+    const location = locationService.createLocation(req.body);
+    res.status(201).json({ data: location });
+  } catch (e) {
+    res.status(500).json({ error: e.error });
+  }
+};
+const findAllLocation = async (req, res) => {
+  try {
+    const locations = locationService.findAllLocation(req.params.id);
+    res.status(200).json({ data: locations });
+  } catch (e) {
+    res.status(500).json({ error: e.error });
+  }
+};
+const findLocation = async (req, res) => {
+  try {
+    const location = locationService.findLocation(req.params.id);
+    res.status(200).json({ data: location });
+  } catch (e) {
+    res.status(500).json({ error: e.error });
+  }
+};
+const deleteLocation = async (req, res) => {
+  try {
+    locationService.deleteLocation(req.params.id);
+    res.status(202).json({ success: "true" });
+  } catch (e) {
+    res.status(500).json({ error: e.error });
+  }
+};
+const updateLocation = async (req, res) => {
+  try {
+    const result = locationService.updateLocation(req.params.id, req.body);
+    res.status(200).json({ data: result });
+  } catch (e) {
+    res.status(500).json({ error: e.error });
+  }
+};
 
 module.exports = {
-    createLocation,
-    findAllLocation,
-    findLocation,
-    deleteLocation,
-    updateLocation
-}
+  createLocation,
+  findAllLocation,
+  findLocation,
+  deleteLocation,
+  updateLocation,
+};
--- MyRoomBE/dao/locationDao.js
+++ MyRoomBE/dao/locationDao.js
@@ -1,32 +1,32 @@
-const models = require("../models")
+const models = require("../models");
 
-const createLocation = async(data)=> {
-    return await models.Location.create(data)
-}
-const findLocationById = async(id)=> {
-    return await models.Location.findAll({
-        where: {roomId: id}
-    })
-}
-const findLocation = async(id) => {
-    return await models.Location.findAll({
-        where: {id}
-    })
-}
-const updateLocation = async(id, data)=> {
-    return await models.Location.update(data, {
-        where: {id}
-    })
-}
-const deleteLocation = async(id) => {
-    return await models.Location.destroy({
-        where: {id}
-    })
-}
+const createLocation = async (data) => {
+  return await models.Location.create(data);
+};
+const findLocationById = async (id) => {
+  return await models.Location.findAll({
+    where: { roomId: id },
+  });
+};
+const findLocation = async (id) => {
+  return await models.Location.findAll({
+    where: { id },
+  });
+};
+const updateLocation = async (id, data) => {
+  return await models.Location.update(data, {
+    where: { id },
+  });
+};
+const deleteLocation = async (id) => {
+  return await models.Location.destroy({
+    where: { id },
+  });
+};
 module.exports = {
-    createLocation,
-    findLocationById,
-    findLocation,
-    updateLocation,
-    deleteLocation
-}
+  createLocation,
+  findLocationById,
+  findLocation,
+  updateLocation,
+  deleteLocation,
+};
