364416dec2dc53a872b4e0909f8e36f671c365df
godjihye <njih112@gmail.com>
2024-11-29 12:26:53 +0900

    find all items by userId

--- MyRoomBE/routers/itemsRouter.js
+++ MyRoomBE/routers/itemsRouter.js
@@ -5,9 +5,9 @@ const router = express.Router();
 router.post("/", itemController.createItem);
 router.get("/:locationId", itemController.findAllItem);
 router.get("/detail/:itemId", itemController.findItem);
-
 router.post("/search", itemController.findItemByName);
 router.delete("/:itemId", itemController.deleteItem);
 router.put("/:itemId", itemController.updateItem);
 router.get("/fav/:userId", itemController.findAllFavItem);
+router.get("/allItem/:userId", itemController.findAllItemByUserId);
 module.exports = router;
