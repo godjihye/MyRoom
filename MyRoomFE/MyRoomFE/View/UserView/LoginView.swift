0a594a75f4724a04e6b9476bea54ad8bbf425b65
godjihye <njih112@gmail.com>
2024-12-05 17:07:27 +0900

    before merge

--- MyRoomFE/MyRoomFE/View/UserView/LoginView.swift
+++ MyRoomFE/MyRoomFE/View/UserView/LoginView.swift
@@ -10,25 +10,32 @@ import SVProgressHUD
 
 struct LoginView: View {
 	@EnvironmentObject var userVM: UserViewModel
-	@State var userID:String = ""
-	@State var password:String = ""
+	@State var email:String = ""
+	@State var password1:String = ""
+	@State var password2:String = ""
 	@State var nickname: String = ""
 	@State var isShowing = false
 	@State var isRegister: Bool = false
 	@State var isVaildEmail: Bool = true
 	@State var isVaildPW: Bool = true
+	@State var isEqualPW: Bool = true
 	@State private var emailOffset: CGFloat = 0
-	 @State private var pwOffset: CGFloat = 0
+	@State private var pwOffset: CGFloat = 0
+	@State private var pwcheckOffset: CGFloat = 0
 	var body: some View {
 		VStack {
+			Text(isRegister ? "회원가입" : "로그인")
+				.font(.largeTitle)
+				.fontWeight(.bold)
 			VStack {
-				CustomTextField(icon: "person.fill", placeholder: "사용자ID", text: $userID)
-					.onChange(of: userID) { oldValue, newValue in
+				CustomTextField(icon: "person.fill", placeholder: "이메일(예시: myroom@gmail.com)", text: $email)
+					.onChange(of: email) { oldValue, newValue in
 						isVaildEmail = true
 					}
 				if !isVaildEmail {
 					Text("이메일 형식으로 입력해주세요.")
 						.font(.footnote)
+						.fontWeight(.bold)
 						.foregroundStyle(.red)
 						.offset(x: emailOffset)
 						.onAppear(perform: {
@@ -39,13 +46,14 @@ struct LoginView: View {
 						})
 						.padding(.leading, 5)
 				}
-				CustomTextField(icon: "lock.fill", placeholder: "비밀번호", text: $password, isSecured: true)
-					.onChange(of: password) { oldValue, newValue in
+				CustomTextField(icon: "lock.fill", placeholder: "비밀번호 (8자 이상으로 입력해주세요.)", text: $password1, isSecured: true)
+					.onChange(of: password1) { oldValue, newValue in
 						isVaildPW = true
 					}
 				if !isVaildPW {
 					Text("8자 이상 입력해주세요.")
 						.font(.footnote)
+						.fontWeight(.bold)
 						.foregroundStyle(.red)
 						.offset(x: pwOffset)
 						.onAppear(perform: {
@@ -57,16 +65,42 @@ struct LoginView: View {
 						.padding(.leading, 5)
 				}
 				if isRegister {
-					CustomTextField(icon: "person.fill", placeholder: "닉네임", text: $nickname)
+					CustomTextField(icon: "lock.fill", placeholder: "비밀번호 확인", text: $password2, isSecured: true)
+						.onChange(of: password2) { oldValue, newValue in
+							isEqualPW = password1.isEqualPW(newValue)
+						}
+					if !isEqualPW {
+						Text("비밀번호가 틀립니다.")
+							.font(.footnote)
+							.fontWeight(.bold)
+							.foregroundStyle(.red)
+							.offset(x: pwcheckOffset)
+							.onAppear(perform: {
+								withAnimation(Animation.easeInOut(duration: 0.1)
+									.repeatCount(10)) {
+										self.pwcheckOffset = -5
+									}
+							})
+							.padding(.leading, 5)
+					}
+					CustomTextField(icon: "person.fill", placeholder: "닉네임(2자 이상 입력해주세요.)", text: $nickname)
 				}
 			}
 
 			if isRegister {
 				WideImageButton(icon: "person.badge.plus", title: "회원가입", backgroundColor: .green) {
-					userVM.signUp(userName: userID, password: password, nickname: nickname)
+					if !email.textFieldValidatorEmail() {
+						isVaildEmail = false
+					} else if !password1.textFieldValidatorPW() {
+						isVaildPW = false
+					} else if !password1.isEqualPW(password2) {
+						isEqualPW = false
+					} else {
+						userVM.signUp(userName: email, password: password1, nickname: nickname)
+					}
 				}
 				.padding()
-				.disabled(userID.isEmpty || password.isEmpty || nickname.isEmpty)
+				.disabled(email.isEmpty || password1.isEmpty || nickname.isEmpty)
 				.alert("회원가입", isPresented: $userVM.isJoinShowing) {
 					Button("확인") {
 						userVM.isJoinShowing = false
@@ -81,13 +115,13 @@ struct LoginView: View {
 				}
 			} else {
 				WideImageButton(icon: "person.badge.key", title: "로  그  인", backgroundColor: .accent) {
-					if !textFieldValidatorEmail(userID) {
+					if !email.textFieldValidatorEmail() {
 						isVaildEmail = false
 					}
-					else if !textFieldValidatorPW(password) {
+					else if !password1.textFieldValidatorPW() {
 						isVaildPW = false
 					} else {
-						userVM.login(userName: userID, password: password)
+						userVM.login(userName: email, password: password1)
 					}
 				}
 				.padding()
@@ -108,20 +142,8 @@ struct LoginView: View {
 	}
 }
 
-func textFieldValidatorEmail(_ string: String) -> Bool {
-		if string.count > 100 {
-				return false
-		}
-		let emailFormat = "(?:[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}" + "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" + "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[\\p{L}0-9](?:[a-" + "z0-9-]*[\\p{L}0-9])?\\.)+[\\p{L}0-9](?:[\\p{L}0-9-]*[\\p{L}0-9])?|\\[(?:(?:25[0-5" + "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" + "9][0-9]?|[\\p{L}0-9-]*[\\p{L}0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" + "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
-		//let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-		let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
-		return emailPredicate.evaluate(with: string)
-}
-func textFieldValidatorPW(_ string: String) -> Bool {
-	string.count >= 8
-}
 
 #Preview {
 	let userVM = UserViewModel()
-	LoginView(userID: "wizard", password: "1234", isRegister: false).environmentObject(userVM)
+	LoginView(email: "wizard", password1: "1234", isRegister: false).environmentObject(userVM)
 }
