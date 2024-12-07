38572c858efe3de96f7156924bc9d963a3fa2475
leesooim <soojeng22@outlook.com>
2024-12-07 19:18:36 +0900

    chat 완료

---
+++ MyRoomFE/MyRoomFE/View/UsedView/UsedAddToDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  UsedAddToDetailView.swift
+//  MyRoomFE
+//
+//  Created by 이수정 on 12/6/24.
+//
+
+import SwiftUI
+
+struct UsedAddToDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsedAddToDetailView()
+}
