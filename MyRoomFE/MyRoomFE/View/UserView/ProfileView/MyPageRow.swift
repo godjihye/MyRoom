8bf55d7836728080277a672c1c4410190551ee63
godjihye <njih112@gmail.com>
2024-12-12 12:54:00 +0900

    fix: MyPage Update

---
+++ MyRoomFE/MyRoomFE/View/UserView/ProfileView/MyPageRow.swift
@@ -0,0 +1,39 @@
+//
+//  MyPageRow.swift
+//  MyRoomFE
+//
+//  Created by jhshin on 12/12/24.
+//
+
+import SwiftUI
+
+struct MyPageRow: View {
+	let icon: String
+	let title: String
+	var backgroundColor: Color = Color.accentColor
+    var body: some View {
+			
+			HStack {
+				Text(icon)
+					.font(.system(size: 20))
+				Text(title)
+					.font(.system(size: 20))
+					.bold()
+				Spacer()
+				Image(systemName: "greaterthan")
+					.renderingMode(.original)
+			}
+			.padding()
+			.background {
+				RoundedRectangle(cornerRadius: 10)
+					.foregroundStyle(backgroundColor)
+					.opacity(0.3)
+			}
+			.foregroundStyle(.black)
+			.padding(.horizontal)
+    }
+}
+
+#Preview {
+	MyPageRow(icon: "🏠", title: "동거인 목록")
+}
--- MyRoomFE/MyRoomFE/View/UserView/ProfileView/MyPageView.swift
+++ MyRoomFE/MyRoomFE/View/UserView/ProfileView/MyPageView.swift
@@ -8,12 +8,54 @@
 import SwiftUI
 
 struct MyPageView: View {
-	
-    var body: some View {
-			ProfileView()
-    }
+	@EnvironmentObject var userVM: UserViewModel
+	var body: some View {
+		NavigationSplitView {
+			ScrollView {
+				ProfileView()
+				Divider()
+				menuView
+				Divider()
+				logoutBtn
+			}
+		} detail: {
+			Text("마이페이지입니다.")
+		}
+		
+	}
+	private var menuView: some View {
+		VStack {
+			NavigationLink {
+				if let userInfo = userVM.userInfo {
+					ProfileEditView(user: userInfo)
+				}
+			} label: {
+				MyPageRow(icon: "👤", title: "회원정보 수정", backgroundColor: .myroom1)
+			}
+			NavigationLink {
+				MateListView()
+			} label: {
+				MyPageRow(icon: "🏠", title: "동거인 목록", backgroundColor: .myroom3)
+			}
+			NavigationLink {
+				ChatListView().environmentObject(ChatViewModel())
+			} label: {
+				MyPageRow(icon: "💬", title: "채팅 목록", backgroundColor: .myroom2)
+			}
+		}
+	}
+	private var logoutBtn: some View {
+		Button {
+			userVM.logout()
+		} label: {
+			Text("로그아웃하기")
+				.foregroundStyle(.gray)
+				.padding()
+		}
+	}
 }
 
 #Preview {
-    MyPageView()
+	let userVM = UserViewModel()
+	MyPageView().environmentObject(userVM)
 }
