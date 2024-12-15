941fd7b4cd0dd04a5f4ce850c9ee59df91c78682
leesooim <soojeng22@outlook.com>
2024-12-15 14:01:46 +0900

    post,used

---
+++ MyRoomFE/MyRoomFE/View/UsedView/UsedSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  UsedSearchView.swift
+//  MyRoomFE
+//
+//  Created by 이수정 on 12/15/24.
+//
+
+import SwiftUI
+
+struct UsedSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsedSearchView()
+}
